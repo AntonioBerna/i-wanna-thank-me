d16219543a405d54c5f781bc8d3709144957daed
AntonioBerna <antoniobernardini@live.com>
2025-01-19 14:25:43 +0100

    edit qr code :tada:

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import qrcode
 from qrcode.constants import ERROR_CORRECT_L
+from PIL import Image
 
 if __name__ == "__main__":
     qr = qrcode.QRCode(
@@ -11,4 +12,18 @@ if __name__ == "__main__":
     qr.add_data("https://antonioberna.github.io/i-wanna-thank-me")
     qr.make(fit=True)
     qr_image = qr.make_image(fill_color="#00833f", back_color="white")
-    qr_image.save("imgs/i-wanna-thank-me.png")
+
+    qr_image = qr_image.convert("RGBA")
+
+    cap_image = Image.open("imgs/cap.png")
+    cap_size = (100, 100)
+    cap_image = cap_image.resize(cap_size, Image.LANCZOS)
+
+    qr_width, qr_height = qr_image.size
+    cap_width, cap_height = cap_image.size
+    position = (
+        (qr_width - cap_width) // 2,
+        (qr_height - cap_height) // 2
+    )
+    qr_image.paste(cap_image, position, cap_image)
+    qr_image.save("imgs/i-wanna-thank-me-with-cap.png")
